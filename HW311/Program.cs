5e66ee5bf84164097ca1d974dec15e5122d64294
Yana <horoshavina_yana@mail.ru>
2023-03-10 16:32:45 +1200

    задача 1

---
+++ HW311/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+
+int GetReverse(int Value)
+{
+    int ValueTMP = Value;
+    int Reverse = 0;
+    while (ValueTMP > 0)
+    {
+        Reverse = Reverse * 10 + ValueTMP % 10;
+        ValueTMP /= 10;
+    }
+    return Reverse;
+}
+
+Console.WriteLine("Введите палиндром");
+int Value = Convert.ToInt32(Console.ReadLine());
+int Reverse = GetReverse(Value);
+ if (Value == Reverse)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
