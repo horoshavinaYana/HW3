1d5460adf0284981895df79680ce2eb500c1d521
Yana <horoshavina_yana@mail.ru>
2023-03-10 16:19:57 +1200

    задача 3

---
+++ HW33/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите длину массива");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] Cube = new int[N];
+for (int index = 0; index < N; index++)
+{
+    Cube[index] = Convert.ToInt32(Math.Pow((index + 1),3));
+    Console.WriteLine(Cube[index]);
+}
